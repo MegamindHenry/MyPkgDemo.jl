a36984f56a7db7d0e81987ef2e33132eda2c3a6e
Xuefeng <hakunamatatahenry@gmail.com>
2020-11-01 11:21:31 -0500

    add first function

--- src/MyPkgDemo.jl
+++ src/MyPkgDemo.jl
@@ -1,5 +1,8 @@
 module MyPkgDemo
 
 # Write your package code here.
+function add_nums(a,b)
+  a+b
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "MyPkgDemo.jl" begin
     # Write your tests here.
+    @test MyPkgDemo.add_nums(2,3) == 5
+    @test MyPkgDemo.add_nums(3,3) == 6
+    @test MyPkgDemo.add_nums(2,4) == 6
+    @test MyPkgDemo.add_nums(4,4) == 7
+
 end
