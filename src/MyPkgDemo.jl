fb3c08e23fa9ecce7b57ad666851170501ea56b4
Xuefeng <hakunamatatahenry@gmail.com>
2020-11-01 11:40:38 -0500

    add first function docs

--- src/MyPkgDemo.jl
+++ src/MyPkgDemo.jl
@@ -1,8 +1,27 @@
 module MyPkgDemo
 
 # Write your package code here.
+"""
+Add numbers.
+"""
+function add_nums end
+
+"""
+    add_nums(a,b)
+
+Add two numbers together.
+"""
 function add_nums(a,b)
   a+b
+end
+
+"""
+    add_nums(a,b,c)
+
+Add three numbers together.
+"""
+function add_nums(a,b,c)
+  a+b+c
 end
 
 end
