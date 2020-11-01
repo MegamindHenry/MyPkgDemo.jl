af59a4980690ea0374c1bed49748605970cc3adf
Xuefeng <hakunamatatahenry@gmail.com>
2020-11-01 11:43:47 -0500

    add test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,8 @@ using Test
     @test MyPkgDemo.add_nums(2,3) == 5
     @test MyPkgDemo.add_nums(3,3) == 6
     @test MyPkgDemo.add_nums(2,4) == 6
-    @test MyPkgDemo.add_nums(4,4) == 7
+    @test MyPkgDemo.add_nums(4,4) == 8
+    @test MyPkgDemo.add_nums(4,3,2) == 9
+    @test MyPkgDemo.add_nums(4,3,4) == 11
 
 end
